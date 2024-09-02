54bcef5e39780cc488802993762f842a64e8a839
AlinaHryshchenko <alinaloshakovarabota@gmail.com>
2024-09-02 02:02:31 +0100

    Initial Commit

---
+++ MarsViewProject/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MarsViewProject
+//
+//  Created by Алина Лошакова on 02.09.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
